907d33ae0b357ac1624bd71594f58b5f8f91c01b
thegreatfeez <adamsafeez4@gmail.com>
2025-09-12 20:47:30 +0100

    fix dist

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import {defineConfig} from "vite"
 
 export default defineConfig({
-	plugins: [
-		
-	]
+  plugins: [
+    
+  ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        project1: 'project-1.html',
+        project2: 'project-2.html', 
+        project3: 'project-3.html'
+      }
+    }
+  }
 })
